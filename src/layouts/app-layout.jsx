be6c90f42f08cca737cc0bf868635ee9c8280207
Abhinav Srivastava <abhinavsrivs05@gmail.com>
2026-07-19 13:42:32 +0530

    Footer fixed

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -10,7 +10,7 @@ const AppLayout = () => {
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
-        © 2024 Made by Abhinav Srivastava to get replies from companies.
+        © 2024 Made by Abhinav Srivastava to get hired.
       </div>
     </div>
   );
